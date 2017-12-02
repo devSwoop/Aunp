269e66ffd3a1d3b149df53d961617543ec8c5e34
Vincent Bertin <vbertin@ideolys.com>
2017-12-02 22:05:47 +0100

    Dependency injection basic handling

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const init 		= require('./lib/initialize');
 module.exports = {
 	init : function (app, directory) {
 		init.initAunp(this, app, directory);
+		init.loadDependencies(this);
 		init.loadComponents(this);
 	}
 }
