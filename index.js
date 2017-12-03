9d99296cb9dd54e8f95b120d5c226439125a50fe
Vincent Bertin <vbertin@ideolys.com>
2017-12-03 16:29:08 +0100

    Merge

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const express 	= require('express');
-const helper 	= require('./lib/helper');
-const path 		= require('path');
-const init 		= require('./lib/initialize');
+const express 			= require('express');
+const helper 			= require('./lib/helper');
+const path 				= require('path');
+const init 				= require('./lib/initialize');
+const stringTemplatter 	= require('./lib/sqlTemplatting');
 
 module.exports = {
 	init : function (app, directory) {
 		init.initAunp(this, app, directory);
 		init.loadDependencies(this);
 		init.loadComponents(this);
-	}
+	},
+	helper 				: helper,
+	stringTemplatter 	: stringTemplatter
 }
